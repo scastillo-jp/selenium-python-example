49402a645d792fdac8c5df1466cc0d1773ef969d
Sandro Castillo <sandro@Sandros-MacBook-Pro.local>
2023-08-31 11:24:03 -0500

    timeout in format string

--- tests/base_test.py
+++ tests/base_test.py
@@ -54,7 +54,7 @@ class BaseTest:
             raise Exception("Incorrect Browser")
 
         self.driver.maximize_window()
-        self.wait = WebDriverWait(self.driver, timeout='15')
+        self.wait = WebDriverWait(self.driver, "15")
         yield self.wait, self.driver
 
         if self.driver is not None:
